b37447aaf2a27e93d3d670dbe0b1d58735b86ff3
Guilherme Denobi <guilherme.denobi46@gmail.com>
2022-11-19 16:14:07 -0300

    first commits

---
+++ ADMIN.sql
@@ -0,0 +1,8 @@
+CREATE TABLE TB_CRUD (id_item int not null, 
+                    nome_carro varchar2(100),
+                    marca_carro varchar2(50),
+                    potencia_carro int,
+                    ano_carro int
+);
+
+SELECT * FROM TB_CRUD
